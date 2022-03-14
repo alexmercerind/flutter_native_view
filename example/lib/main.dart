924907d85185375e3b814763d56a7325003cca68
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-03-15 02:22:06 +0530

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,26 +19,64 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final controller = NativeViewController(
-    windowHandle: FindWindow(
-      'Notepad'.toNativeUtf16(),
-      nullptr,
+  final controller = ScrollController();
+  final controllers = [
+    NativeViewController(
+      handle: FindWindow(
+        nullptr,
+        'VLC Media Player'.toNativeUtf16(),
+      ),
     ),
-  );
+    NativeViewController(
+      handle: FindWindow(
+        'Notepad'.toNativeUtf16(),
+        nullptr,
+      ),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: Handle scrolling & positioning of child HWNDs automatically.
+    controller.addListener(() {
+      for (final controller in controllers) {
+        controller.refresh();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('flutter_native_view'),
+          elevation: 0.0,
         ),
-        body: Center(
-          child: NativeView(
-            controller: controller,
-            width: 640.0,
-            height: 480.0,
-          ),
+        body: ListView(
+          controller: controller,
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: NativeView(
+                controller: controllers[0],
+                width: 640.0,
+                height: 480.0,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: NativeView(
+                controller: controllers[1],
+                width: 640.0,
+                height: 480.0,
+              ),
+            ),
+          ],
         ),
       ),
     );
