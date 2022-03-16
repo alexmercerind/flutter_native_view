7953d0a8206fbef8e94f86600e6a5af3fde715fb
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-03-16 16:18:24 +0530

    example: add flutternativeview::CreateNativeViewContainer call

--- example/windows/runner/main.cpp
+++ example/windows/runner/main.cpp
@@ -2,11 +2,12 @@
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
 
+#include "flutter_native_view/flutter_native_view_plugin.h"
 #include "flutter_window.h"
 #include "utils.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
+                      _In_ wchar_t* command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -19,8 +20,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   flutter::DartProject project(L"data");
 
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
@@ -31,6 +31,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
+
+  flutternativeview::CreateNativeViewContainer();
 
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0)) {
