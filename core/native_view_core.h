0ec63e6e25131d7b650884373f7af60ef0b3a803
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-05-25 14:44:27 +0530

    chore: re-license to MIT

--- core/native_view_core.h
+++ core/native_view_core.h
@@ -1,21 +1,10 @@
-/// This file is part of flutter_native_view
-/// (https://github.com/alexmercerind/flutter_native_view).
-///
-/// Copyright (C) 2022 Hitesh Kumar Saini <saini123hitesh@gmail.com>
-///
-/// flutter_native_view is free software: you can redistribute it and/or modify
-/// it under the terms of the GNU General Public License as published by the
-/// Free Software Foundation, either version 3 of the License, or (at your
-/// option) any later version.
-///
-/// flutter_native_view is distributed in the hope that it will be useful, but
-/// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-/// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-/// more details.
-///
-/// You should have received a copy of the GNU General Public License along with
-/// flutter_native_view. If not, see <https://www.gnu.org/licenses/>.
-///
+// This file is a part of libmpv.dart
+// (https://github.com/alexmercerind/libmpv.dart).
+//
+// Copyright (c) 2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+// All rights reserved.
+// Use of this source code is governed by MIT license that can be found in the
+// LICENSE file.
 
 #ifndef NATIVE_VIEW_CORE_H_
 #define NATIVE_VIEW_CORE_H_
